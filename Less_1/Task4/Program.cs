fa3f45f6f2e0a1f2c724603765c62b9fc94b37ea
Ivan <khjohn135@gmail.com>
2024-02-01 16:12:51 +0600

    add Lesson1:Task4

---
+++ Less_1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 45;
+int b = 7;
+int c = 5;
+int d = 23;
+int e = 4;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+System.Console.WriteLine(max);
