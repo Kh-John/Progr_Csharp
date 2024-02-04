9b9bbb2de72d0176e7f5693c78a224165640f689
Ivan <khjohn135@gmail.com>
2024-02-04 09:39:40 +0600

    add lesson2:task1

---
+++ Less_2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] arr = new int[5];
+
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 8;
+arr[4] = 1;
+
+System.Console.WriteLine(arr [0]);
+System.Console.WriteLine(arr [1]);
+System.Console.WriteLine(arr [2]);
+System.Console.WriteLine(arr [3]);
+System.Console.WriteLine(arr [4]);
