2c54a653970d4325b62b461696473481b91ad9e2
Ivan <khjohn135@gmail.com>
2024-02-09 23:01:11 +0600

    add seminarHW:zadacha2

---
+++ seminar2_HW/Zadacha2/Program.cs
@@ -0,0 +1,32 @@
+﻿int x = 5;
+int y = -3;
+
+if (x > 0 & y > 0)
+
+{
+    System.Console.WriteLine("1");
+}
+
+if (x < 0 & y > 0)
+
+{
+    System.Console.WriteLine("2");
+}
+
+if (x < 0 & y < 0)
+
+{
+    System.Console.WriteLine("3");
+}
+
+if (x > 0 & y < 0)
+{
+    System.Console.WriteLine("4");
+}
+else
+{
+    System.Console.WriteLine("Одна из координат равна нулю!");
+}
+
+
+
