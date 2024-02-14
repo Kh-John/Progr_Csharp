dc3bbb14f736a0aa532aa90d8325d87215bf69ff
Ivan <khjohn135@gmail.com>
2024-02-14 12:47:13 +0600

    add less3:task3

--- Less_3/Task3/Program.cs
+++ Less_3/Task3/Program.cs
@@ -1,7 +1,22 @@
-﻿void ZeroEventElements(int[]arr)
+﻿void ZeroEventElements(int[] arr)
 {
-    for (int i=0; i>arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
     }
 }
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 5, 3, 9, 5 };
+ZeroEventElements(array);
+PrintArray(array);
