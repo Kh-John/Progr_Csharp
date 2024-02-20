bc93f0d25a569697c7793b72761d1bc694386bb5
Ivan <khjohn135@gmail.com>
2024-02-20 16:52:42 +0600

    add less2:task5

---
+++ Less_2/Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 8, 5, 7, 2, 6 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
