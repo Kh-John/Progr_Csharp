69c321524c39c0ce708b0d509b185d8c991af38e
Ivan <khjohn135@gmail.com>
2024-02-20 15:16:52 +0600

    add less2:task4

---
+++ Less_2/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 7;
+int b = 4;
+int c = 8;
+int d = 5;
+int e = 9;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.Write("Maximum is:");
+Console.Write(max);
