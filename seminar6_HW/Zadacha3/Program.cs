98e95c6c40be7b65d9985c59f06ad3a1e9813ff7
Ivan <khjohn135@gmail.com>
2024-02-24 11:31:52 +0600

    add seminar:zadacha3

---
+++ seminar6_HW/Zadacha3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = "5446445";
+
+        string result = IsPalindrome(inputString) ? "да" : "нет";
+
+        Console.WriteLine($"Строка является палиндромом? {result}");
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
