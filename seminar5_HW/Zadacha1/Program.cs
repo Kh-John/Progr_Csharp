0e6f155fd2b681c7fb49b2dbbfd82203cfae3596
Ivan <khjohn135@gmail.com>
2024-02-24 10:51:41 +0600

    add seminar5_HW

---
+++ seminar5_HW/Zadacha1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем двумерный массив символов
+        char[,] charArray = {
+            {'H', 'e', 'l', 'l', 'o'},
+            {'W', 'o', 'r', 'l', 'd'}
+        };
+
+        // Создаем строку из символов массива
+        string str = GetStringFromCharArray(charArray);
+
+        // Выводим результат
+        Console.WriteLine("Строка из символов массива: " + str);
+    }
+
+    // Метод для создания строки из символов массива
+    static string GetStringFromCharArray(char[,] arr)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+        char[] charArray = new char[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                charArray[index] = arr[i, j];
+                index++;
+            }
+        }
+
+        return new string(charArray);
+    }
+}
---
+++ seminar5_HW/Zadacha2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем исходную строку
+        string inputString = "HeLLoWoRLd";
+
+        // Формируем строку с заменой заглавных букв на строчные
+        string lowercaseString = ConvertToLowerCase(inputString);
+
+        // Выводим результат
+        Console.WriteLine("Исходная строка: " + inputString);
+        Console.WriteLine("Строка с заменой заглавных букв на строчные: " + lowercaseString);
+    }
+
+    // Метод для замены заглавных букв на строчные
+    static string ConvertToLowerCase(string input)
+    {
+        char[] chars = input.ToCharArray(); // Преобразуем строку в массив символов
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            // Проверяем, является ли символ заглавной буквой
+            if (char.IsUpper(chars[i]))
+            {
+                // Если символ заглавная буква, заменяем его на соответствующую строчную букву
+                chars[i] = char.ToLower(chars[i]);
+            }
+        }
+
+        // Преобразуем массив символов обратно в строку
+        return new string(chars);
+    }
+}
