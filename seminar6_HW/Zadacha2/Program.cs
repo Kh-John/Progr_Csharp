a7af0f080f0fa9a1e902237d0748498e16be531d
Ivan <khjohn135@gmail.com>
2024-02-24 11:21:50 +0600

    add seminar:zadacha2

---
+++ seminar6_HW/Zadacha2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = "aBcD1ef!-";
+
+        string resultString = ConvertToLowerCase(inputString);
+
+        Console.WriteLine(resultString);
+    }
+
+    static string ConvertToLowerCase(string input)
+    {
+        string result = "";
+
+        foreach (char c in input)
+        {
+            if (char.IsUpper(c))
+            {
+                result += char.ToLower(c);
+            }
+            else
+            {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+}
